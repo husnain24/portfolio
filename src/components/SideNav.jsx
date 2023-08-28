8365abed94ba21a24056648ed77e0c106a719371
Husnain <98628692+husnain24@users.noreply.github.com>
2023-08-27 22:20:25 -0600

    Removed resume section

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -58,14 +58,6 @@ const SideNav = () => {
           </a>
           <a
             onClick={handleNav}
-            href='#main'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
-          >
-            <BsPerson size={20} />
-            <span className='pl-4'>Resume</span>
-          </a>
-          <a
-            onClick={handleNav}
             href='#contact'
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
           >
@@ -96,12 +88,6 @@ const SideNav = () => {
             className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
           >
             <AiOutlineProject size={20} />
-          </a>
-          <a
-            href='#main'
-            className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
-          >
-            <BsPerson size={20} />
           </a>
           <a
             href='#contact'
