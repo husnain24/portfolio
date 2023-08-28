a6e3082336362acddfc1a67a5d443ff512af3610
Husnain <98628692+husnain24@users.noreply.github.com>
2023-08-27 22:52:59 -0600

    Added profile pic to the main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,5 @@
 import { FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import profilePic from '../assets/profilePic.jpg';
 
 const MainPage = () => {
   return (
@@ -8,13 +9,31 @@ const MainPage = () => {
         src='https://media.istockphoto.com/id/1185747322/photo/blue-mesh-gradient-blurred-motion-abstract-background.jpg?b=1&s=612x612&w=0&k=20&c=EU2Le9ohrdXYHEVfYAmiHAhYPk7eyRj5XTnJwctd7tA='
         alt=''
       />
-      <div className='w-full h-screen absolute top-0 left-0'>
+      <div className='flex flex-col  items-center justify-center w-full h-screen absolute top-0 left-0 lg:items-start'>
+        {/* <div className='w-[200px] h-[200px] flex items-center justify-center pt-16'>
+          <div className='border border-white rounded-full overflow-hidden w-[200px] h-[200px]'>
+            <img
+              src={profilePic}
+              alt=''
+              className='object-cover w-full h-full'
+            />
+          </div>
+        </div> */}
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+          <div className='w-[200px] h-[200px] flex items-center justify-center pb-16 lg:ml-9'>
+            <div className='border border-white rounded-full overflow-hidden w-[200px] h-[200px]'>
+              <img
+                src={profilePic}
+                alt=''
+                className='object-cover w-full h-full'
+              />
+            </div>
+          </div>
           <h1 className='sm:text-5xl text-4xl pl-9 font-bold text-white'>
             I'm Husnain Ahmad
           </h1>
           <h2 className='sm:text-xl text-2xl pt-4 pl-9 text-white'>
-            I'm a passionate programmer and here's some of my work!
+            I'm a passionate programmer and here's my portfolio!
           </h2>
           <div className='flex justify-between pt-6 pl-9 max-w-[200px] w-full'>
             <a href='https://www.youtube.com/' target='_blank'>
