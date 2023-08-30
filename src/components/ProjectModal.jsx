41d1d018aa17c78c8e28e7b1522431eded327187
Husnain <98628692+husnain24@users.noreply.github.com>
2023-08-29 22:03:42 -0600

    Fixed the images on the projects. Added better text descriptions for each project.

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,78 +1,14 @@
-// import React from 'react';
-
-// const ProjectModal = ({ isOpen, onClose, title, details }) => {
-//   if (!isOpen) return null;
-
-//   return (
-//     <div className='fixed inset-0 flex items-center justify-center z-50'>
-//       <div
-//         onClick={onClose}
-//         className='modal-overlay fixed inset-0 bg-black opacity-50'
-//       ></div>
-//       <div className='modal z-10 bg-white p-4 rounded-lg w-96'>
-//         <button
-//           className='modal-close absolute top-2 right-2 text-gray-600'
-//           onClick={onClose}
-//         >
-//           &times;
-//         </button>
-//         <h2 className='modal-title text-xl font-semibold text-gray-800 mb-2'>
-//           {title}
-//         </h2>
-//         <p className='modal-details text-gray-600'>{details}</p>
-//         {/* Add more content here, like images and additional information */}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProjectModal;
-
-// import { Carousel } from 'react-responsive-carousel';
-// import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import the carousel styles
-
-// const ProjectModal = ({ isOpen, onClose, title, details, images }) => {
-//   if (!isOpen) return null;
-
-//   return (
-//     <div className='fixed inset-0 flex items-center justify-center z-50'>
-//       <div
-//         className='modal-overlay fixed inset-0 bg-black opacity-50'
-//         onClick={onClose}
-//       ></div>
-//       <div className='modal z-10 bg-white p-4 rounded-lg w-96'>
-//         <button
-//           className='modal-close absolute top-2 right-2 text-gray-600'
-//           onClick={onClose}
-//         >
-//           &times;
-//         </button>
-//         <h2 className='modal-title text-xl font-semibold text-gray-800 mb-2'>
-//           {title}
-//         </h2>
-//         {/* Carousel */}
-//         <Carousel showThumbs={false}>
-//           {images.map((image, index) => (
-//             <div key={index}>
-//               <img src={image} alt={`Image ${index}`} />
-//             </div>
-//           ))}
-//         </Carousel>
-//         {/* Project Description */}
-//         <div className='mt-4'>
-//           <p className='modal-details text-gray-600'>{details}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProjectModal;
-
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const ProjectModal = ({ isOpen, onClose, title, details, images }) => {
+const ProjectModal = ({
+  isOpen,
+  onClose,
+  title,
+  details,
+  images,
+  description,
+}) => {
   if (!isOpen) return null;
 
   return (
@@ -93,26 +29,24 @@ const ProjectModal = ({ isOpen, onClose, title, details, images }) => {
         </h2>
         <Carousel showThumbs={false}>
           {images.map((image, index) => (
-            <div key={index}>
+            <div className='w-' key={index}>
               <img src={image} alt={`Image ${index}`} />
             </div>
           ))}
         </Carousel>
         <div className='mt-4'>
-          <p className='modal-details text-gray-600'>{details}</p>
+          <p className='modal-details text-gray-600'>{`Technologies: ${details}`}</p>
         </div>
-        <div className='mt-4'>
-          <p className='text-gray-800'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec
-            justo eu ligula interdum aliquam. Fusce euismod dapibus turpis et
-            convallis. Nullam euismod turpis nec ultrices lacinia.
-          </p>
-          <p className='text-gray-800 mt-2'>
-            Sed finibus enim vel neque ultricies, in consectetur lorem
-            vestibulum. Nulla facilisi. Fusce et tellus eu mi congue vulputate.
-            Suspendisse at turpis auctor, auctor tellus eget, volutpat libero.
-          </p>
+        <div className='mt-4 text-xl'>
+          <ul>
+            {description.map((text) => {
+              return <li>{text}</li>;
+            })}
+          </ul>
         </div>
+        {/* <div className='mt-4 text-xl'>
+          <p className='text-gray-800'>{description}</p>
+        </div> */}
       </div>
     </div>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,32 +2,57 @@ import { useState } from 'react';
 import ProjectItem from './ProjectItem';
 import snakeGame from '../assets/SnakeGame.png';
 import timeManagementApp from '../assets/TimeManagement.png';
-import dataStructureTeachingApp from '../assets/DataStructureTeachingApp.png';
+import spriteEditor from '../assets/SpriteEditor.png';
+import spreadsheet from '../assets/SpreadsheetApp.png';
 import ProjectModal from './ProjectModal';
 
 // Random
-import rand1 from '../assets/rand1.png';
-import rand2 from '../assets/rand2.png';
-import rand3 from '../assets/rand3.jpg';
+import snake1 from '../assets/SnakeProject/wholeScreen.png';
+import snake2 from '../assets/SnakeProject/server.png';
+import snake3 from '../assets/SnakeProject/deadSnake.png';
 
 const data = [
+  {
+    img: spriteEditor,
+    title: 'Sprite Editor App',
+    details: 'C++ | QT Creator',
+    images: [snake1, snake2, snake3],
+    description: [
+      'Allows users to load, save and edit the sprite pixel art while also providing them with an assortment of drawing tools.',
+      'Also allows them to preview their sprite animations and configure different sizes.',
+      'Collaborated with a team utilizing proper software practices to build.',
+    ],
+    // 'Allows users to load, save and edit the sprite pixel art while also providing them with an assortment of drawing tools. Also allows them to preview their sprite animations and configure different sizes. Collaborated with a team utilizing proper software practices to build.',
+  },
   {
     img: snakeGame,
     title: 'Snake Server/Client Game',
     details: 'C# | .NET MAUI',
-    images: [rand1, rand2, rand3],
+    images: [snake1, snake2, snake3],
+    description: [
+      'Online Multiplayer game of Snake with a server and game client. Collaborated with a friend to design the server and client architecture for this game utilizing the TCP/IP protocol.',
+      'It’s a cross platform game given Microsoft’s .NET MAUI framework. Utilizes parallel-computing/multi-threading so it can handle multiple concurrent players. The server provides editable modular game settings as well as different game modes.',
+    ],
+    // 'Online Multiplayer game of Snake with a server and game client. Collaborated with a friend to design the server and client architecture for this game utilizing the TCP/IP protocol. It’s a cross platform game given Microsoft’s .NET MAUI framework. Utilizes parallel-computing/multi-threading so it can handle multiple concurrent players. The server provides editable modular game settings as well as different game modes.',
   },
   {
     img: timeManagementApp,
     title: 'Time Management Suite',
     details: 'JavaScript | HTML | CSS',
-    images: [rand1, rand2, rand3],
+    images: [snake1, snake2, snake3],
+    description: [
+      'Provides user with a timer, a music selection, and a complete to-do list to stay productive.',
+      'Enhance productivity by providing users with a medium of organizing their tasks and marking them complete as they go.',
+      'Provides users a timer they can set to stay on task as well as some study music playing options. Utilized local storage to keep progress and save the user’s tasks. Hosted through Replit',
+    ],
+    // 'Provides user with a timer, a music selection, and a complete to-do list to stay productive. Enhance productivity by providing users with a medium of organizing their tasks and marking them complete as they go. Provides users a timer they can set to stay on task as well as some study music playing options. Utilized local storage to keep progress and save the user’s tasks. Hosted through Replit',
   },
   {
-    img: dataStructureTeachingApp,
-    title: 'Data Structure Educational App',
-    details: 'C++ | QT Creator',
-    images: [rand1, rand2, rand3],
+    img: spreadsheet,
+    title: 'Spreadsheet Application',
+    details: 'C# | .NET MAUI',
+    images: [snake1, snake2, snake3],
+    description: ['testing'],
   },
 ];
 
@@ -67,6 +92,7 @@ const Projects = () => {
         title={modalData.title}
         details={modalData.details}
         images={modalData.images}
+        description={modalData.description}
       />
     </div>
   );
