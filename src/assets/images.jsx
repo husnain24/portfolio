7103cc9a50d2d42df70b9b698ee47fa2c2f24ce1
Husnain Ahmad <husnain4491@gmail.com>
2024-08-08 22:39:48 -0600

    Added more image resources

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -17,14 +17,30 @@ import spreadsheet from './SpreadsheetApp/SpreadsheetApp.png';
 import spreadsheet1 from './SpreadsheetApp/nextTab.png';
 import spreadsheet2 from './SpreadsheetApp/save.png';
 
+import gradingApp1 from './AcademicGradingPortal/index.png';
+import gradingApp2 from './AcademicGradingPortal/assignments.png';
+import gradingApp3 from './AcademicGradingPortal/grading.png';
+import gradingApp4 from './AcademicGradingPortal/profile.png';
+
+import drawingApp1 from './MobileDrawingApp/drawing.png';
+import drawingApp2 from './MobileDrawingApp/login.png';
+import drawingApp3 from './MobileDrawingApp/local.png';
+import drawingApp4 from './MobileDrawingApp/cloud.png';
+
 const snakeImages = [snakeGame, snake1, snake2, snake3];
 const timeManagementImages = [timeManagementApp, time1, time2, time3];
 const spriteEditorImages = [spriteEditor, sprite1, sprite2, sprite3];
 const spreadsheetImages = [spreadsheet, spreadsheet1, spreadsheet2];
+const gradingPortalImages = [gradingApp1, gradingApp2, gradingApp3, gradingApp4];
+const mobileDrawingApp = [drawingApp1, drawingApp2, drawingApp3, drawingApp4];
+
+
 
 export default {
   snakeImages,
   timeManagementImages,
   spriteEditorImages,
   spreadsheetImages,
+  gradingPortalImages,
+  mobileDrawingApp,
 };
